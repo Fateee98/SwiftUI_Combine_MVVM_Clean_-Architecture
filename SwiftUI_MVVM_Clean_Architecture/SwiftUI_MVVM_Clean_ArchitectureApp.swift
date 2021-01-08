7717c5a496a2c94fc571d4a27f95a30c1ac4d2d1
Fate <luongquanh2204@gmail.com>
2021-01-08 19:23:22 +0700

    Initial Commit

---
+++ SwiftUI_MVVM_Clean_Architecture/SwiftUI_MVVM_Clean_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_Clean_ArchitectureApp.swift
+//  SwiftUI_MVVM_Clean_Architecture
+//
+//  Created by Fate on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_Clean_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
